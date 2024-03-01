2cac86abef9c8598db40622e4abcf55c35f74e93
Cordelia Hu <156957266+coralcordelia@users.noreply.github.com>
2024-03-01 03:36:53 -0500

    Update 05_extract.py

--- 05_extract.py
+++ 05_extract.py
@@ -4,3 +4,15 @@
 #  Objective: Extract course titles from 
 #  the data you cleaned.
 # -----------------------------------------------
+
+import json
+
+def store_json(data, file):
+    with open(file, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+        print('Data saved to: ', file)
+
+with open("cleaned_bodies.json", "r", encoding='utf-8') as infile:
+    bodies = json.load(infile)
+    titles = [f"{i[0]} {i[1]}: {i[2]}" for i in bodies] # stores the titles of the courses into a file
+    store_json(titles, 'titles.json')
