01b27a8b823ab0ae9059df1d804622bcf03c337a
Cordelia Hu <156957266+coralcordelia@users.noreply.github.com>
2024-03-01 03:38:23 -0500

    Update 09_pipeline.py

--- 09_pipeline.py
+++ 09_pipeline.py
@@ -10,3 +10,40 @@
 #  previous files, consolidating the 
 #  results into one sequence.
 # -----------------------------------------------
+"""
+Required modules/things to install before running:
+- os
+- ssl
+- plotly
+- bs4
+Run time is at most a few minutes
+Notice that many files will get loaded while running the program. This is also why filedump is empty; running the program will fill the thing
+"""
+
+
+import os
+
+os.system('npm i common-words --save-dev') # pulls the common words thing 
+
+
+
+
+print('Pulling data from online...')
+os.system('python 01_pull.py')
+print('Combining data...')
+os.system('python 02_combine.py')
+print('Parsing data...')
+os.system('python 03_parse.py')
+print('Cleaning data...')
+os.system('python 04_clean.py')
+print('Extracting data...')
+os.system('python 05_extract.py')
+print('Generating word frequency...')
+os.system('python 06_frequency.py')
+print('Generating visualization...')
+print('If the program pauses for too long, do CONTROL+C then run 07_visualization.py afterwards to see the visualization!')
+os.system('python 07_visualization.py')
+print('Exporting data...')
+os.system('python 08_export.py')
+   
+
