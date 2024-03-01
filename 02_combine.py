8732bd122c3dd24a9a3e9efbeb0612ebe4bde291
Cordelia Hu <156957266+coralcordelia@users.noreply.github.com>
2024-03-01 03:35:56 -0500

    Update 02_combine.py

--- 02_combine.py
+++ 02_combine.py
@@ -7,3 +7,11 @@
 #  Tools/Resources: Concatenate HTML text using 
 #  python or javascript.
 # -----------------------------------------------
+import glob, os
+
+os.chdir(".")
+
+with open('combined.html', 'w') as outfile:
+    for file in glob.glob("filedump/*.html"):
+        with open(file) as infile:
+            outfile.write(infile.read()) # writes the html script into the combined file
