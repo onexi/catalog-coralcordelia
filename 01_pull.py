7e0d3eeb793c8187db448920eab18e7d77aea7be
Cordelia Hu <156957266+coralcordelia@users.noreply.github.com>
2024-03-01 03:34:46 -0500

    Updated 01_pull.py

--- 01_pull.py
+++ 01_pull.py
@@ -13,3 +13,46 @@
 #     NE: https://catalog.northeastern.edu/course-descriptions
 # -----------------------------------------------
 
+
+import urllib.request
+import ssl
+from random import *
+import time
+
+from bs4 import BeautifulSoup
+
+
+
+context = ssl._create_unverified_context()
+
+
+def pull (url):
+    data = urllib.request.urlopen(url, context=context).read()
+    text = data.decode('utf-8')
+    return text
+
+
+def store (data, file):
+    f = open('filedump/' + file, 'w')
+    f.write(data)
+    f.close()
+    print(f'File saved as: {file}')
+
+homepage = pull('https://catalog.northeastern.edu/course-descriptions/')
+homepage = homepage.replace('\n', '').replace('\r', '')
+soup = BeautifulSoup(homepage, 'html.parser')
+urls = []
+for letter in soup.find("div", id="atozindex").contents[1::2]:
+    for item in letter.contents:
+        print('https://catalog.northeastern.edu' + item.contents[0]['href'])
+        urls.append('https://catalog.northeastern.edu' + item.contents[0]['href'].rstrip('/'))
+
+
+for url in urls:
+    ind = url.rfind('/') + 1
+    data = pull(url)
+    file = url[ind:] + '.html'
+    print('Fetching ' + file)
+    store(data, file)
+    time.sleep(random() * 0.2 + 0.2)
+
