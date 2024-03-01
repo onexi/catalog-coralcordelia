665c8c83ecaf1d92861d8cc4d5bbf766fa548e83
Cordelia Hu <156957266+coralcordelia@users.noreply.github.com>
2024-03-01 03:37:47 -0500

    Update 07_visualization.py

--- 07_visualization.py
+++ 07_visualization.py
@@ -10,3 +10,21 @@
 #     Chart.JS, https://www.chartjs.org/
 #     Google Charts, https://developers.google.com/chart/
 # -----------------------------------------------
+
+import json
+import plotly.express as px
+len_graph = 500
+keep_common_words = False
+
+common_words_file = json.load(open('node_modules/common-words/words.json'))
+common_words = {i['word'] for i in common_words_file} # loads a collection of some of the most commonly used words in English.
+
+with open("word_frequencies_in_titles.json", "r", encoding='utf-8') as infile:
+    word_frequencies = json.load(infile)
+    word_frequencies = {i: word_frequencies[i] for i in word_frequencies.keys() if i not in common_words or keep_common_words} # we trim out common words
+    fig = px.bar(x=list(word_frequencies.keys())[:len_graph], y = [i['frequency'] for i in list(word_frequencies.values())[:len_graph]])
+    fig.update_xaxes(type='category') #since this is a catagorical graph
+    fig.show()
+    pass
+    
+
